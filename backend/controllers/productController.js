f00a24a4f7697f05996606b0feb987f23691ad94
abhishek <abhishek123@gmail.com>
2024-03-05 11:25:58 +0530

    product api completed

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,73 @@
-const getAllProducts = (req, res)=>{
-    res.status(200).json({message: "Route is working fine"});
+import { Product } from "../models/product.model.js";
+
+// Create Product - Admin
+export const createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product
+  });
+};
+
+// Get all product
+export const getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products
+  });
+};
+
+//Update Product -- Admin
+export const updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found"
+    });
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false
+  });
+  res.status(200).json({
+    success: true,
+    product
+  });
+};
+
+//Delete Product
+export const deleteProduct = async (req, res, next)=>{
+  const product = await Product.findById(req.params.id);
+  if(!product)
+  {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found"
+    });
+  }
+  await product.deleteOne();
+  res.status(200).json({
+    success:true,
+    message: "Product deleted successfully"
+  })
+
 }
-export default getAllProducts;
+
+//Get single product details
+export const getProductDetails = async(req, res, next)=>{
+  const product = await Product.findById(req.params.id);
+  if(!product)
+  {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found"
+    })
+  }
+  res.status(200).json({
+    success: true,
+    product
+  })
+}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,7 +1,17 @@
-import express from 'express';
-import getAllProducts from '../controllers/productController.js';
+import express from "express";
+import {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getProductDetails
+} from "../controllers/productController.js";
 const router = express.Router();
 
-router.route('/products').get(getAllProducts);
+router.route("/products").get(getAllProducts);
+
+router.route("/product/new").post(createProduct);
+
+router.route("/product/:id").put(updateProduct).delete(deleteProduct).get(getProductDetails);
 
 export default router;
