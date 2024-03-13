84f69258a33298287c2e7ca2939ca7e086ef938a
Akash Bheke <akashab2362@gmail.com>
2024-03-13 17:13:16 +0530

    Error handling for backend completed

--- backend/middleware/catchAsyncErrors.js
+++ backend/middleware/catchAsyncErrors.js
@@ -1,6 +1,6 @@
-// export const catchAsyncError = (theFunc) => (req, res, next) => {
-//   Promise.resolve(theFunc(req, res, next)).catch(next);
-// };
+export const catchAsyncError = (theFunc) => (req, res, next) => {
+  Promise.resolve(theFunc(req, res, next)).catch(next);
+};
 
 // export const catchAsyncError = (fn) => {
 //   (req, res, next) => {
