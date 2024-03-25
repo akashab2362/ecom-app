e285848643e8b379e72d53c75ca8b6c666ec829b
Akash Bheke <akashab2362@gmail.com>
2024-03-25 20:01:23 +0530

    Routes defined in index.js for proper readability and consistency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,13 @@
 import "./App.css";
-import Login from "./Components/login";
-import Register from "./Components/register";
-import Home from "./Components/home";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./Components/navbar";
+import Home from "./Components/home";
 import Footer from "./Components/Footer";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <>
-          <Navbar />
-          <Home />
-        </>
-      ),
-    },
-    {
-      path: "/login",
-      element: (
-        <>
-          <Navbar />
-          <Login />
-        </>
-      ),
-    },
-    {
-      path: "/register",
-      element: (
-        <>
-          <Navbar />
-          <Register />
-        </>
-      ),
-    },
-  ]);
   return (
     <div className="App">
-      <RouterProvider router={router} />
+      <Navbar/>
+      <Home/>
       <Footer/>
     </div>
   );
